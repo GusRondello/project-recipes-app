3753a1481cf072db0fb020a7d94dc5403d5415fb
Thiago <bodnarthiago@gmail.com>
2022-04-26 18:45:29 -0300

    Tela de Login. Requisito 5. Validação do Login feitan

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Login() {
+  const [userInfo, setUserInfo] = useState({
+    email: '',
+    password: '',
+  });
+  const [isButtonDisabled, setButtonDisabled] = useState(true);
+
+  const handleInputChange = ({ target }) => {
+    const { name, value } = target;
+    setUserInfo({
+      ...userInfo,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    const validateForm = () => {
+      const { email, password } = userInfo;
+      const regex = /\S+@\S+\.\S+/;
+      const minPasswordLength = 6;
+      const validateEmail = regex.test(email);
+      const validatePassword = password.length > minPasswordLength;
+      if (validateEmail && validatePassword) {
+        setButtonDisabled(false);
+      } else {
+        setButtonDisabled(true);
+      }
+    };
+    validateForm();
+  }, [userInfo]);
+
   return (
     <>
       <h1>Login</h1>
@@ -9,19 +39,22 @@ function Login() {
           name="email"
           type="email"
           data-testid="email-input"
-          // onChange={ handleInputChange }
+          onChange={ handleInputChange }
+          value={ userInfo.email }
           placeholder="E-mail"
         />
         <input
           name="password"
           type="password"
           data-testid="password-input"
-          // onChange={ handleInputChange }
+          onChange={ handleInputChange }
+          value={ userInfo.password }
           placeholder="Password"
         />
         <button
           type="button"
           data-testid="login-submit-btn"
+          disabled={ isButtonDisabled }
         >
           Enter
         </button>
