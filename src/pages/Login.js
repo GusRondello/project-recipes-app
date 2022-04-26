d041cae818b752cb483ae2aad7a563c49e0e42f4
Thiago <bodnarthiago@gmail.com>
2022-04-26 18:25:06 -0300

    Tela de Login. Requisitos 2, 3, 4. Criados os inputs de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,31 @@ import React from 'react';
 
 function Login() {
   return (
-    <div>Login</div>
+    <>
+      <h1>Login</h1>
+      <form>
+        <input
+          name="email"
+          type="email"
+          data-testid="email-input"
+          // onChange={ handleInputChange }
+          placeholder="E-mail"
+        />
+        <input
+          name="password"
+          type="password"
+          data-testid="password-input"
+          // onChange={ handleInputChange }
+          placeholder="Password"
+        />
+        <button
+          type="button"
+          data-testid="login-submit-btn"
+        >
+          Enter
+        </button>
+      </form>
+    </>
   );
 }
 
