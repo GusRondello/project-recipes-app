e5c88775b06f1b06bc12ea8e7cb51dc20910b854
Lindsey Fontana <lindseyfontanas@gmail.com>
2022-04-26 20:36:05 -0300

    Header-Requisito 9 concluído

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
+import '../styles/Header.css';
 
-function Header() {
+function Header({ pageTitle, componentName }) {
   return (
-    <div>Header</div>
+    <header>
+      <img src={ profileIcon } data-testid="profile-top-btn" alt="Profile Icon" />
+      <h1 data-testid="page-title">{ pageTitle }</h1>
+      {componentName === 'drinks' || componentName === 'foods'
+        ? <img src={ searchIcon } data-testid="search-top-btn" alt="Search Button" />
+        : null}
+    </header>
   );
 }
+
+Header.propTypes = {
+  pageTitle: PropTypes.string.isRequired,
+  componentName: PropTypes.string.isRequired,
+};
 
 export default Header;
