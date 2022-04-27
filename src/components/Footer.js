ed91acec219b760496e03bd3da0359588377c2cc
Thiago <bodnarthiago@gmail.com>
2022-04-27 18:35:36 -0300

    Footer: Requisitos 22, 23 e 24, Botões redirecionam para a rota correta

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import drinkIcon from '../images/drinkIcon.svg';
+import { Link } from 'react-router-dom';
+import drinksIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 import '../styles/Footer.css';
@@ -7,30 +8,15 @@ import '../styles/Footer.css';
 function Footer() {
   return (
     <footer data-testid="footer">
-      <button
-        type="button"
-        data-testid="drinks-bottom-btn"
-        src={ drinkIcon }
-      >
-        <img src={ drinkIcon } alt="drink icon" />
-
-      </button>
-      <button
-        type="button"
-        data-testid="explore-bottom-btn"
-        src={ exploreIcon }
-      >
-        <img src={ exploreIcon } alt="explore icon" />
-
-      </button>
-      <button
-        type="button"
-        data-testid="food-bottom-btn"
-        src={ mealIcon }
-      >
-        <img src={ mealIcon } alt="meal icon" />
-
-      </button>
+      <Link to="/drinks">
+        <img src={ drinksIcon } data-testid="drinks-bottom-btn" alt="Drink Icon" />
+      </Link>
+      <Link to="/explore">
+        <img src={ exploreIcon } data-testid="explore-bottom-btn" alt="Explore Icon" />
+      </Link>
+      <Link to="/foods">
+        <img src={ mealIcon } data-testid="food-bottom-btn" alt="Meal Icon" />
+      </Link>
     </footer>
   );
 }
