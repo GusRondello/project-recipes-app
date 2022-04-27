c177de7653397d6bb68a86fc3902e9cf37045c5f
Lindsey Fontana <lindseyfontanas@gmail.com>
2022-04-27 11:37:27 -0300

    Barra de busca - Requisito 13 cocnluído

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,7 +32,39 @@ function Header({ pageTitle, componentName }) {
           : null}
       </header>
       {searchInput
-        && <input data-testid="search-input" className="search-input" />}
+        && (
+          <>
+            <input data-testid="search-input" className="search-input" />
+            <button type="button" data-testid="exec-search-btn">Search</button>
+            <br />
+            <label htmlFor="ingredient">
+              <input
+                type="radio"
+                name="filter"
+                id="ingredient"
+                data-testid="ingredient-search-radio"
+              />
+              Ingredient
+            </label>
+            <label htmlFor="name">
+              <input
+                type="radio"
+                name="filter"
+                id="name"
+                data-testid="name-search-radio"
+              />
+              Name
+            </label>
+            <label htmlFor="first-letter">
+              <input
+                type="radio"
+                name="filter"
+                id="first-letter"
+                data-testid="first-letter-search-radio"
+              />
+              First Letter
+            </label>
+          </>)}
     </>
   );
 }
