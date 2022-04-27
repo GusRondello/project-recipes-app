9dc198cb2affa6801d0cdab97cd8346f1ae23fb6
Lindsey Fontana <lindseyfontanas@gmail.com>
2022-04-27 09:50:52 -0300

    Header-Requisito 11 concluído

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 import '../styles/Header.css';
@@ -7,7 +8,9 @@ import '../styles/Header.css';
 function Header({ pageTitle, componentName }) {
   return (
     <header>
-      <img src={ profileIcon } data-testid="profile-top-btn" alt="Profile Icon" />
+      <Link to="/profile">
+        <img src={ profileIcon } data-testid="profile-top-btn" alt="Profile Icon" />
+      </Link>
       <h1 data-testid="page-title">{ pageTitle }</h1>
       {componentName === 'drinks'
       || componentName === 'foods'
