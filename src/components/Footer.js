ecf74af2e6b8b038334721a59f92c3b6c0517644
Thiago <bodnarthiago@gmail.com>
2022-04-27 17:24:34 -0300

    requisito 19. Botões do menu inferior criados

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,29 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer data-testid="footer">
+      <button
+        type="button"
+        data-testid="drinks-bottom-btn"
+      >
+        drinks
+
+      </button>
+      <button
+        type="button"
+        data-testid="explore-bottom-btn"
+      >
+        explore
+
+      </button>
+      <button
+        type="button"
+        data-testid="food-bottom-btn"
+      >
+        food
+
+      </button>
+    </footer>
   );
 }
 
