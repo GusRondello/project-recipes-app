74a2c09378fc313368941d11835f57d098653625
Thiago <bodnarthiago@gmail.com>
2022-04-27 19:37:40 -0300

    Página de detalhes: elementos criados nas páginas de detalhes de comidas e bebidas

--- src/pages/Drinks/DrinkDetails.js
+++ src/pages/Drinks/DrinkDetails.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 function DrinkDetails() {
   return (
-    <div>DrinkDetails</div>
+    <section>
+      <img src="" alt="" data-testid="recipe-photo" />
+      <h2 data-testid="recipe-title">Titulo</h2>
+      <button type="button" data-testid="share-btn">Share</button>
+      <button type="button" data-testid="favorite-btn">Favorite</button>
+      <p data-testid="recipe-category">Lorem Ipsum</p>
+      <li data-testid={ `${index}-ingredient-name-and-measure` } />
+      <p data-testid="instructions">Lorem Ipsum</p>
+      <div data-testid={ `${index}-recomendation-card` } />
+      <button type="button" data-testid="start-recipe-btn">Start Recipe</button>
+    </section>
   );
 }
 
--- src/pages/Foods/FoodDetails.js
+++ src/pages/Foods/FoodDetails.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 function FoodDetails() {
   return (
-    <div>FoodDetails</div>
+    <section>
+      <img src="" alt="" data-testid="recipe-photo" />
+      <h2 data-testid="recipe-title">Titulo</h2>
+      <button type="button" data-testid="share-btn">Share</button>
+      <button type="button" data-testid="favorite-btn">Favorite</button>
+      <p data-testid="recipe-category">Lorem Ipsum</p>
+      <li data-testid={ `${index}-ingredient-name-and-measure` } />
+      <p data-testid="instructions">Lorem Ipsum</p>
+      <iframe title="Video" width="853" height="480" data-testid="video" src="" />
+      <div data-testid={ `${index}-recomendation-card` } />
+      <button type="button" data-testid="start-recipe-btn">Start Recipe</button>
+    </section>
   );
 }
 
