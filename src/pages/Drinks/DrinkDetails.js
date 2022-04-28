1a69acc1756b8577e779a276ce2d9f2466b0af1c
Thiago <bodnarthiago@gmail.com>
2022-04-28 19:52:27 -0300

    tela de detalhes: requisito 35 feito. Preenchea tela com os dados da API

--- src/pages/Drinks/DrinkDetails.js
+++ src/pages/Drinks/DrinkDetails.js
@@ -11,22 +11,51 @@ function DrinkDetails() {
       const fetchDrinkById = async () => {
         const response = await fetch(URL);
         const { drinks } = await response.json();
-        setDrink(drinks);
+        setDrink(drinks[0]);
       };
       fetchDrinkById();
     }, [URL],
   );
 
+  const {
+    strDrinkThumb,
+    strDrink,
+    strAlcoholic,
+    strInstructions,
+  } = drink;
+
+  const getIngredientsAndMeasure = () => {
+    const twenty = 20;
+    const ingredientsAndMeasure = [];
+    for (let i = 1; i < twenty; i += 1) {
+      if (drink[`strIngredient${i}`]) {
+        const li = (
+          <li
+            key={ i }
+            data-testid={ `${i - 1}-ingredient-name-and-measure` }
+          >
+            {drink[`strIngredient${i}`]}
+            {' '}
+            -
+            {' '}
+            {drink[`strMeasure${i}`]}
+          </li>);
+        ingredientsAndMeasure.push(li);
+      }
+    }
+    return ingredientsAndMeasure;
+  };
+
   return (
     <section>
-      <img src="" alt="" data-testid="recipe-photo" />
-      <h2 data-testid="recipe-title">Titulo</h2>
+      <img src={ strDrinkThumb } alt={ strDrink } data-testid="recipe-photo" />
+      <h2 data-testid="recipe-title">{ strDrink }</h2>
       <button type="button" data-testid="share-btn">Share</button>
       <button type="button" data-testid="favorite-btn">Favorite</button>
-      <p data-testid="recipe-category">Lorem Ipsum</p>
-      {/* <li data-testid={ `${index}-ingredient-name-and-measure` } /> */}
-      <p data-testid="instructions">Lorem Ipsum</p>
-      {/* <div data-testid={ `${index}-recomendation-card` } /> */}
+      <p data-testid="recipe-category">{ strAlcoholic }</p>
+      <ul>{ getIngredientsAndMeasure() }</ul>
+      <p data-testid="instructions">{ strInstructions }</p>
+      <div data-testid="0-recomendation-card" />
       <button type="button" data-testid="start-recipe-btn">Start Recipe</button>
     </section>
   );
--- src/pages/Foods/FoodDetails.js
+++ src/pages/Foods/FoodDetails.js
@@ -5,29 +5,63 @@ function FoodDetails() {
   const [recipe, setRecipe] = useState([]);
   const { id } = useParams();
   const URL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+  const {
+    strMealThumb,
+    strMeal,
+    strCategory,
+    strInstructions,
+    strYoutube,
+  } = recipe;
 
   useEffect(
     () => {
       const fetchRecipeById = async () => {
         const response = await fetch(URL);
         const { meals } = await response.json();
-        setRecipe(meals);
+        setRecipe(meals[0]);
       };
       fetchRecipeById();
     }, [URL],
   );
 
+  const getIngredientsAndMeasure = () => {
+    const twenty = 20;
+    const ingredientsAndMeasure = [];
+    for (let i = 1; i < twenty; i += 1) {
+      if (recipe[`strIngredient${i}`]) {
+        const li = (
+          <li
+            key={ i }
+            data-testid={ `${i - 1}-ingredient-name-and-measure` }
+          >
+            {recipe[`strIngredient${i}`]}
+            {' '}
+            -
+            {' '}
+            {recipe[`strMeasure${i}`]}
+          </li>);
+        ingredientsAndMeasure.push(li);
+      }
+    }
+    return ingredientsAndMeasure;
+  };
   return (
     <section>
-      <img src="" alt="" data-testid="recipe-photo" />
-      <h2 data-testid="recipe-title">Titulo</h2>
+      <img src={ strMealThumb } alt={ strMeal } data-testid="recipe-photo" />
+      <h2 data-testid="recipe-title">{ strMeal }</h2>
       <button type="button" data-testid="share-btn">Share</button>
       <button type="button" data-testid="favorite-btn">Favorite</button>
-      <p data-testid="recipe-category">Lorem Ipsum</p>
-      {/* <li data-testid={ `${index}-ingredient-name-and-measure` } /> */}
-      <p data-testid="instructions">Lorem Ipsum</p>
-      <iframe title="Video" width="853" height="480" data-testid="video" src="" />
-      {/* <div data-testid={ `${index}-recomendation-card` } /> */}
+      <p data-testid="recipe-category">{ strCategory }</p>
+      <ul>{getIngredientsAndMeasure()}</ul>
+      <p data-testid="instructions">{ strInstructions }</p>
+      <iframe
+        title={ strMeal }
+        width="341"
+        height="160"
+        data-testid="video"
+        src={ String(strYoutube).replace('watch?v=', 'embed/') }
+      />
+      <div data-testid="0-recomendation-card" />
       <button type="button" data-testid="start-recipe-btn">Start Recipe</button>
     </section>
   );
