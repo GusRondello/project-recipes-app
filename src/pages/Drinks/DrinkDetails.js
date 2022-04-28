48400c7dcf39d4c454205f00f558e4983e5171b7
Thiago <bodnarthiago@gmail.com>
2022-04-28 14:34:35 -0300

    Tela de Detalhes: requisito 34. Realiza chamada da API utilizando o id passado via url

--- src/pages/Drinks/DrinkDetails.js
+++ src/pages/Drinks/DrinkDetails.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 function DrinkDetails() {
+  const [drink, setDrink] = useState([]);
+  const { id } = useParams();
+  const URL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+
+  useEffect(
+    () => {
+      const fetchDrinkById = async () => {
+        const response = await fetch(URL);
+        const { drinks } = await response.json();
+        setDrink(drinks);
+      };
+      fetchDrinkById();
+    }, [URL],
+  );
+
   return (
     <section>
       <img src="" alt="" data-testid="recipe-photo" />
@@ -8,9 +24,9 @@ function DrinkDetails() {
       <button type="button" data-testid="share-btn">Share</button>
       <button type="button" data-testid="favorite-btn">Favorite</button>
       <p data-testid="recipe-category">Lorem Ipsum</p>
-      <li data-testid={ `${index}-ingredient-name-and-measure` } />
+      {/* <li data-testid={ `${index}-ingredient-name-and-measure` } /> */}
       <p data-testid="instructions">Lorem Ipsum</p>
-      <div data-testid={ `${index}-recomendation-card` } />
+      {/* <div data-testid={ `${index}-recomendation-card` } /> */}
       <button type="button" data-testid="start-recipe-btn">Start Recipe</button>
     </section>
   );
--- src/pages/Foods/FoodDetails.js
+++ src/pages/Foods/FoodDetails.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 function FoodDetails() {
+  const [recipe, setRecipe] = useState([]);
+  const { id } = useParams();
+  const URL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+
+  useEffect(
+    () => {
+      const fetchRecipeById = async () => {
+        const response = await fetch(URL);
+        const { meals } = await response.json();
+        setRecipe(meals);
+      };
+      fetchRecipeById();
+    }, [URL],
+  );
+
   return (
     <section>
       <img src="" alt="" data-testid="recipe-photo" />
@@ -8,10 +24,10 @@ function FoodDetails() {
       <button type="button" data-testid="share-btn">Share</button>
       <button type="button" data-testid="favorite-btn">Favorite</button>
       <p data-testid="recipe-category">Lorem Ipsum</p>
-      <li data-testid={ `${index}-ingredient-name-and-measure` } />
+      {/* <li data-testid={ `${index}-ingredient-name-and-measure` } /> */}
       <p data-testid="instructions">Lorem Ipsum</p>
       <iframe title="Video" width="853" height="480" data-testid="video" src="" />
-      <div data-testid={ `${index}-recomendation-card` } />
+      {/* <div data-testid={ `${index}-recomendation-card` } /> */}
       <button type="button" data-testid="start-recipe-btn">Start Recipe</button>
     </section>
   );
