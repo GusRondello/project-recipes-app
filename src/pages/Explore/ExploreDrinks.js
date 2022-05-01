c74409de6050011c2bb6407f9b3d0a079c5a5432
Lindsey Fontana <lindseyfontanas@gmail.com>
2022-05-01 12:16:49 -0300

    Tela de explorar - Requisito 72-73 concluídos

--- src/pages/Explore/ExploreDrinks.js
+++ src/pages/Explore/ExploreDrinks.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 
 function ExploreDrinks() {
+  const history = useHistory();
   const componentName = 'explore-drinks';
   const pageTitle = 'Explore Drinks';
+
+  const handleClick = ({ target }) => {
+    const buttonName = target.name;
+    if (buttonName === 'byIngredient') {
+      history.push('/explore/drinks/ingredients');
+    }
+  };
+
   return (
     <div>
       <Header pageTitle={ pageTitle } componentName={ componentName } />
       <button
         type="button"
         data-testid="explore-by-ingredient"
+        name="byIngredient"
+        onClick={ handleClick }
       >
         By Ingredient
       </button>
--- src/pages/Explore/ExploreFoods.js
+++ src/pages/Explore/ExploreFoods.js
@@ -1,28 +1,46 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 
 function ExploreFoods() {
+  const history = useHistory();
   const componentName = 'explore-foods';
   const pageTitle = 'Explore Foods';
+
+  const handleClick = ({ target }) => {
+    const buttonName = target.name;
+    if (buttonName === 'byIngredient') {
+      history.push('/explore/foods/ingredients');
+    }
+    if (buttonName === 'byNationality') {
+      history.push('/explore/foods/nationalities');
+    }
+  };
+
   return (
     <div>
       <Header pageTitle={ pageTitle } componentName={ componentName } />
       <button
         type="button"
         data-testid="explore-by-ingredient"
+        name="byIngredient"
+        onClick={ handleClick }
       >
         By Ingredient
       </button>
       <button
         type="button"
         data-testid="explore-by-nationality"
+        name="byNationality"
+        onClick={ handleClick }
       >
         By Nationality
       </button>
       <button
         type="button"
         data-testid="explore-surprise"
+        name="surpriseMe"
       >
         Surprise me!
       </button>
