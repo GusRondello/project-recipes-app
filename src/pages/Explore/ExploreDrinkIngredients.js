30de1a8f6a74ddfb383e3b8d49a3f77a50599a8e
Lindsey Fontana <lindseyfontanas@gmail.com>
2022-05-01 14:28:01 -0300

    Tela de explorar ingredientes - Requisito 75-76 concluídos

--- src/pages/Explore/ExploreDrinkIngredients.js
+++ src/pages/Explore/ExploreDrinkIngredients.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 
+const DOZE = 12;
+
 function ExploreDrinkIngredients() {
+  const [ingredients, changeIngredients] = useState([]);
   const componentName = 'explore-drinks-ingredients';
   const pageTitle = 'Explore Ingredients';
+
+  useEffect(() => {
+    const getIngredients = async () => {
+      const request = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list');
+      const ingredientsResponse = await request.json();
+      const firstIngredients = ingredientsResponse.drinks.slice(0, DOZE);
+      const ingredientsList = firstIngredients
+        .map((ingredient) => ingredient.strIngredient1);
+      changeIngredients(ingredientsList);
+    };
+
+    getIngredients();
+  }, []);
+
   return (
     <div>
       <Header pageTitle={ pageTitle } componentName={ componentName } />
+      {ingredients !== []
+        && ingredients.map((ingredient, index) => (
+          <div
+            key={ index }
+            data-testid={ `${index}-ingredient-card` }
+          >
+            <img
+              src={ `https://www.thecocktaildb.com/images/ingredients/${ingredient}-Small.png` }
+              data-testid={ `${index}-card-img` }
+              alt="ingredient"
+            />
+            <h1 data-testid={ `${index}-card-name` }>{ingredient}</h1>
+          </div>
+        )) }
+      <img
+        src={ `https://www.thecocktaildb.com/images/ingredients/${ingredients[1]}-Small.png` }
+        alt="ingredient"
+      />
       <Footer />
     </div>
   );
--- src/pages/Explore/ExploreFoodIngredients.js
+++ src/pages/Explore/ExploreFoodIngredients.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 
+const DOZE = 12;
+
 function ExploreFoodIngredients() {
+  const [ingredients, changeIngredients] = useState([]);
   const componentName = 'explore-foods-ingredients';
   const pageTitle = 'Explore Ingredients';
+
+  useEffect(() => {
+    const getIngredients = async () => {
+      const request = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list');
+      const ingredientsResponse = await request.json();
+      changeIngredients(ingredientsResponse.meals.slice(0, DOZE));
+    };
+
+    getIngredients();
+  }, []);
+
   return (
     <div>
       <Header pageTitle={ pageTitle } componentName={ componentName } />
+      {ingredients !== []
+        && ingredients.map((ingredient, index) => (
+          <div key={ ingredient.idIngredient } data-testid={ `${index}-ingredient-card` }>
+            <img
+              src={ `https://www.themealdb.com/images/ingredients/${ingredient.strIngredient}-Small.png` }
+              data-testid={ `${index}-card-img` }
+              alt="ingredient"
+            />
+            <h1 data-testid={ `${index}-card-name` }>{ingredient.strIngredient}</h1>
+          </div>
+        )) }
       <Footer />
     </div>
   );
