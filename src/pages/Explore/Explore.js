2fba74bbde471fddcbd293665ce73c98017e48f8
Lindsey Fontana <lindseyfontanas@gmail.com>
2022-05-01 11:06:06 -0300

    Tela de explorar - Requisito 69 concluído

--- src/pages/Explore/Explore.js
+++ src/pages/Explore/Explore.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 
 function Explore() {
+  const history = useHistory();
   const componentName = 'explore';
   const pageTitle = 'Explore';
+
+  const handleClickFoodsButton = () => {
+    history.push('/explore/foods');
+  };
+
+  const handleClickDrinksButton = () => {
+    history.push('/explore/drinks');
+  };
+
   return (
     <div>
       <Header pageTitle={ pageTitle } componentName={ componentName } />
-      <button type="button" data-testid="explore-foods">Explore Foods</button>
-      <button type="button" data-testid="explore-drinks">Explore Drinks</button>
+      <button
+        type="button"
+        data-testid="explore-foods"
+        onClick={ handleClickFoodsButton }
+      >
+        Explore Foods
+      </button>
+      <button
+        type="button"
+        data-testid="explore-drinks"
+        onClick={ handleClickDrinksButton }
+      >
+        Explore Drinks
+      </button>
       <Footer />
     </div>
   );
