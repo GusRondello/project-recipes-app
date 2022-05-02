a8a2961b0755b6f4ff763dbd38f6806935d18792
Lindsey Fontana <lindseyfontanas@gmail.com>
2022-05-02 16:43:18 -0300

    Tela de explorar nacionalidades - Requisito 79 concluído

--- src/pages/Explore/ExploreFoodNationalities.js
+++ src/pages/Explore/ExploreFoodNationalities.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import FoodCard from '../../components/FoodCard';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
-import FoodCard from '../../components/FoodCard';
 
 const DOZE = 12;
 
 function ExploreFoodNationalities() {
   const [initialRecipes, changeInitialRecipes] = useState([]);
   const [nationalities, changeNationalities] = useState([]);
+  const [selectedNationality, changeSelectedNationalities] = useState('');
   const componentName = 'explore-nationalities';
   const pageTitle = 'Explore Nationalities';
 
@@ -17,6 +18,7 @@ function ExploreFoodNationalities() {
       const recipes = await request.json();
       changeInitialRecipes(recipes.meals.slice(0, DOZE));
     };
+
     const getNationaliries = async () => {
       const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list');
       const data = await response.json();
@@ -27,14 +29,33 @@ function ExploreFoodNationalities() {
     getFirstRecipeMeals();
   }, []);
 
+  useEffect(() => {
+    if (selectedNationality !== '') {
+      const getRecipes = async () => {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${selectedNationality}`);
+        const recipes = await response.json();
+        changeInitialRecipes(recipes.meals.slice(0, DOZE));
+      };
+      getRecipes();
+    }
+  }, [selectedNationality]);
+
+  const handleChange = ({ target }) => {
+    changeSelectedNationalities(target.value);
+  };
+
   return (
     <div>
       <Header pageTitle={ pageTitle } componentName={ componentName } />
-      <select data-testid="explore-by-nationality-dropdown">
+      <select data-testid="explore-by-nationality-dropdown" onChange={ handleChange }>
         <option>All</option>
         {nationalities
           .map((nationality) => (
-            <option key={ nationality } data-testid={ `${nationality}-option` }>
+            <option
+              key={ nationality }
+              value={ nationality }
+              data-testid={ `${nationality}-option` }
+            >
               {nationality}
             </option>))}
       </select>
