7eaf3b057339faa5e3afaf4054173270961968bd
GusRond <gustavorondello99@outlook.com>
2022-05-03 14:12:50 -0300

    [Grupo 22] Tela de Perfil completa

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,16 +1,53 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-function Profile() {
+function Profile({ history }) {
+  const userInfo = window.localStorage.getItem('user');
+  const userObj = JSON.parse(userInfo);
+
   const component = 'profile';
   const pageTitle = 'Profile';
+
+  const handleLogoutButton = () => {
+    window.localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <div>
-      <Header pageTitle={ pageTitle } component={ component } />
+      <Header pageTitle={ pageTitle } componentName={ component } />
+      <h1 data-testid="profile-email">
+        {userObj.email}
+      </h1>
+      <Link
+        to="/done-recipes"
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+      </Link>
+      <Link
+        to="/favorite-recipes"
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </Link>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ handleLogoutButton }
+      >
+        Logout
+      </button>
       <Footer />
     </div>
   );
 }
+
+Profile.propTypes = {
+  history: PropTypes.objectOf(PropTypes.any).isRequired,
+};
 
 export default Profile;
